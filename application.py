26a6882ff9486cb5a516d93c8e3172f1b7a4886f
Nikhil varma <nikhilvarma7127@gmail.com>
2021-07-02 23:17:31 +0530

    final login update

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import Flask,request
 from flask_restful import Api, Resource
 from models import Database
 from hashlib import sha256
+import jwt
 
 ################################################################
 
@@ -32,8 +33,10 @@ class Login(Resource):
         if data.get('password') and data.get('email') and data.get('key'):
             if data['key'] == 'e75774559e4c4532a313769f7294d70b':
                 user = db.findUser(data['email'].strip())[0]
-                if user == sha256(data['password'].strip().encode().hexdigest()):
-                    return {'status': 'done'}
+                if user == sha256(data['password'].strip().encode()).hexdigest():
+                    data.pop('key')
+                    k = jwt.encode(data, '65aea63a16bf4e1ba415f0215cbfee55')
+                    return {'token':k}
                 return {'message': 'incorrect credentials'}
             return {"message":"UnAuthorized"}
         return {"message":"Not valid inputs"}
