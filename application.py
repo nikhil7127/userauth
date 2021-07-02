8d5fa12e97435521a257f4f674fed7c8a80161ef
Nikhil varma <nikhilvarma7127@gmail.com>
2021-07-02 22:26:06 +0530

    init

---
+++ application.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask,request
+from flask_restful import Api, Resource
+from models import Database
+from hashlib import sha256
+
+################################################################
+
+application = Flask(__name__)
+api = Api(application)
+db = Database()
+
+################################################################
+
+
+class Register(Resource):
+    def get(self):
+        data = request.args.to_dict()
+        if data.get('username') and data.get('password') and data.get('email') and data.get('key'):
+            if data['key'] == 'e75774559e4c4532a313769f7294d70b':
+                k = db.add((data['username'],data['email'],sha256(data['password'].encode()).hexdigest()))
+                return {"message":str(k)}
+            return {"message":"UnAuthorized"}
+        return {"message":"Not valid inputs"}
+
+################################################################
+
+
+class Login(Resource):
+    def get(self):
+        data = request.args.to_dict()
+        if data.get('password') and data.get('email') and data.get('key'):
+            if data['key'] == 'e75774559e4c4532a313769f7294d70b':
+                return data
+            return {"message":"UnAuthorized"}
+        return {"message":"Not valid inputs"}
+
+################################################################
+
+
+class Show(Resource):
+    def get(self):
+        k = db.fetch()
+        z = {}
+        for ind,a in  enumerate(k):
+            z[ind] = a
+        return z
+
+
+api.add_resource(Login, "/auth/login")
+api.add_resource(Register, "/auth/register")
+api.add_resource(Show,"/")
+
+
+if __name__ == '__main__':
+    application.run(debug=True, host="0.0.0.0", port=int(os.environ.get("POST", 8080)))
+
+    db.close()
+
---
+++ models.py
@@ -0,0 +1,51 @@
+import pymysql
+
+class Database:
+
+  insertCMD = 'insert into users(username,email,password) values(%s,%s,%s)'
+  deleteCMD = 'delete from users where email = %s'
+  updateCMD = 'update users set username = %s where username = %s'
+
+  def __init__(self):
+    try:
+      self.db = pymysql.connect(host='database.cueluwhjjs5i.us-west-1.rds.amazonaws.com',user='admin',password='nikhilD8',charset='utf8mb4')
+      self.cursor = self.db.cursor()
+      self.cursor.execute('use nikhil')
+    except:
+      print("can't connect")
+  
+  def fetch(self):
+    self.cursor.execute("select * from users")
+    return self.cursor.fetchall()
+  
+  def add(self,data):
+    try:
+      self.cursor.execute(self.insertCMD,data)
+      self.cursor.connection.commit()
+      return 1
+    except Exception as e:
+      return {'error':str(e)}
+  
+  def findUser(self,email):
+    self.cursor.execute("select password from users where email= %s",(email,))
+    return self.cursor.fetchone()
+  
+  def delete(self,mail):
+    try:
+      self.cursor.execute(self.deleteCMD,(mail,))
+      self.cursor.connection.commit()
+      return 1
+    except Exception as e:
+      return {'error':str(e)}
+  
+  def update(self,From,To):
+    try:
+      self.cursor.execute(updateCMD,(To,From))
+      self.cursor.connection.commit()
+      return 1
+    except Exception as e:
+      return {'error':str(e)}
+  
+  def close(self):
+    self.cursor.connection.close()
+
